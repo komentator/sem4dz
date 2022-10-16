b662d3aa36d3e3d8cf3883495e32836572030c07
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-16 22:30:04 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 7, 8
+
+
+*/ 
+int[] FillArray(int length)
+{
+    int[] collection = new int[length];
+    for(int i = 0; i < collection.Length; i++) collection[i] = new Random().Next(-100,100);
+    return collection;
+}
+
+void PrintArrya(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++) Console.Write(col[position] + " ");
+}
+
+PrintArrya(FillArray(8));
